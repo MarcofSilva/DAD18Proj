2ef508b979e7b4fd850b0f64c4b072c499c3a86a
JoaoPBSousa <joao.sousa9397@gmail.com>
2018-11-06 15:35:30 +0000

    server strating to ack, nonce done i think

--- DIDA-TUPLE-XL/Server/ServerService.cs
+++ DIDA-TUPLE-XL/Server/ServerService.cs
@@ -11,21 +11,59 @@ namespace Server
     class ServerService : MarshalByRefObject, IServerService
     {
         private Server _server;
+        //tem de se fazer lock disto
+        private Dictionary<string, long> _nocestorage = new Dictionary<string, long>();
 
         public ServerService(Server server) {
             _server = server;
         }
 
-        public void Write(ArrayList tuple, string clientURL) {
-            _server.write(tuple);
+        private bool validRequest(string clientURL, long nonce) {
+            //se nunca apareceu vai ser adicionado
+            if (!_nocestorage.ContainsKey(clientURL)) {
+                _nocestorage.Add(clientURL, nonce);
+                return true;
+            }
+            else {//ja apareceu
+                long a;
+                if (_nocestorage.TryGetValue(clientURL, out a)) {
+                    if (nonce > a) {
+                        _nocestorage.Remove(clientURL);
+                        _nocestorage.Add(clientURL, nonce);
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            //never reaches here
+            return false;
         }
 
-        public void Read(ArrayList tuple, string clientURL) {
-            _server.read(tuple);
+        public void Write(ArrayList tuple, string clientURL, long nonce) {
+            if (validRequest(clientURL,nonce)) {//sucess
+                _server.write(tuple);
+            }
+            else {//request duplicated
+
+            }
         }
 
-        public void Take(ArrayList tuple, string clientURL) {
-            _server.take(tuple);
+        public void Read(ArrayList tuple, string clientURL, long nonce) {
+            if (validRequest(clientURL, nonce)) {//sucess
+                _server.take(tuple);
+            }
+            else {//request duplicated
+
+            }
+        }
+
+        public void Take(ArrayList tuple, string clientURL, long nonce) {
+            if (validRequest(clientURL, nonce)) {//sucess
+                _server.take(tuple);
+            }
+            else {//request duplicated
+
+            }
         }
     }
 }
