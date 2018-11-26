c93254608f9e01b49855a00c44b2c561ae421700
JoaoPBSousa <joao.sousa9397@gmail.com>
2018-11-26 22:46:27 +0000

    started SMR

---
+++ DIDA-TUPLE-XL/Client/API_SMR.cs
@@ -0,0 +1,64 @@
+﻿using RemoteServicesLibrary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client {
+    class API_SMR : TupleSpaceAPI {
+
+        private const int defaultPort = 8085;
+        private TcpChannel channel;
+        private List<IServerService> serverRemoteObjects;
+        private int numServers;
+
+        private string url;
+
+        public API_SMR(string URL) {
+            serverRemoteObjects = prepareForRemoting(ref channel, URL);
+            numServers = serverRemoteObjects.Count;
+
+            url = URL;
+        }
+
+        public delegate void writeDelegate(ArrayList tuple, string url, long nonce);
+        public delegate List<ArrayList> readDelegate(ArrayList tuple, string url, long nonce);
+        public delegate List<ArrayList> takeDelegate(ArrayList tuple, string url, long nonce);
+        //public delegate void takeRemoveDelegate(ArrayList tuple, string url, long nonce);
+
+        public override void Write(ArrayList tuple) {
+            WaitHandle[] handles = new WaitHandle[numServers];
+            try {
+                for (int i = 0; i < numServers; i++) {
+                    IServerService remoteObject = serverRemoteObjects[i];
+                    writeDelegate writeDel = new writeDelegate(remoteObject.Write);
+                    IAsyncResult ar = writeDel.BeginInvoke(tuple, url, nonce, null, null);
+                    handles[i] = ar.AsyncWaitHandle;
+                }
+                if (!WaitHandle.WaitAll(handles, 1000)) {
+                    Write(tuple);
+                }
+                else {
+                    nonce += 1;
+                }
+            }
+            catch (SocketException) {
+                //TODO
+                throw new NotImplementedException();
+            }
+        }
+        public override ArrayList Read(ArrayList tuple) {
+            throw new NotImplementedException();
+        }
+
+        public override ArrayList Take(ArrayList tuple) {
+            throw new NotImplementedException();
+        }
+    }
+}
