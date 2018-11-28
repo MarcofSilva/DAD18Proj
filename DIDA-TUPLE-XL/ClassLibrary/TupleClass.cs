59d0deda9f36704e8c5598b28ced6f59180a9bf7
JoaoPBSousa <joao.sousa9397@gmail.com>
2018-11-28 19:12:06 +0000

    started tuple class

---
+++ DIDA-TUPLE-XL/ClassLibrary/TupleClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace ClassLibrary {
+    [Serializable]
+    public class TupleClass {
+
+        //List<?> tuple = new List<?>();        
+        public ArrayList _tuple = new ArrayList();
+        private int _size;
+
+        public int Size {
+            get {
+                return _size;
+            }
+
+            set {
+                _size = value;
+            }
+        }
+
+        //if 2 tuples are the same
+        public bool Equals(TupleClass tuple) {
+            if (_size != tuple.Size) {
+
+            }
+
+            return false;
+        }
+
+
+        //if 2 tuples match, takes care of wildcards
+        public bool Matches(TupleClass obj) {
+
+            return false;
+        }
+
+        public void Add(Object o) {
+            _tuple.Add(o);
+            _size += 1;
+        }
+    }
+}
