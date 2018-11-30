d6f446cd8f918d299be4b94463ae717e9c324436
Marco Silva <marcofsilva@tecnico.ulisboa.pt>
2018-11-30 22:23:49 +0000

    Add Client remote class and interface

---
+++ DIDA-TUPLE-XL/Client/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary;
+using RemoteServicesLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    public class ClientService : IClientService{
+
+        private TupleSpaceAPI _clientAPI;
+
+        public ClientService(TupleSpaceAPI clientAPI) {
+            _clientAPI = clientAPI;
+        }
+
+        public TupleClass TakeRemove(List<TupleClass> tupleSubset) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DIDA-TUPLE-XL/RemoteServicesLibrary/IClientService.cs
@@ -0,0 +1,13 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServicesLibrary {
+    public interface IClientService {
+
+        TupleClass TakeRemove(List<TupleClass> tupleSubset);
+    }
+}
