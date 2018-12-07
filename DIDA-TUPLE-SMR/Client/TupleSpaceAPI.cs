497847b6710554db132dd72f93c7545cba6fcfac
JoaoPBSousa <joao.sousa9397@gmail.com>
2018-12-07 17:53:04 +0000

    problems with freeze

--- DIDA-TUPLE-SMR/Client/TupleSpaceAPI.cs
+++ DIDA-TUPLE-SMR/Client/TupleSpaceAPI.cs
@@ -18,7 +18,7 @@ namespace Client {
     public abstract class TupleSpaceAPI {
 
         private long _operationNonce;
-        private List<IServerService> serverRemoteObjects = new List<IServerService>();
+        public List<IServerService> serverRemoteObjects = new List<IServerService>();
 
         public TupleSpaceAPI() {
             _operationNonce = 0;
@@ -46,7 +46,7 @@ namespace Client {
 
         public delegate List<string> requestViewDelegate();
 
-        protected IServerService prepareForRemoting(ref TcpChannel channel, string URL) {
+        protected List<IServerService> prepareForRemoting(ref TcpChannel channel, string URL) {
             string[] urlSplit = URL.Split(new Char[] { '/', ':' }, StringSplitOptions.RemoveEmptyEntries);
             int port;
             Console.WriteLine(URL);
@@ -59,14 +59,11 @@ namespace Client {
             Console.WriteLine("Hello! I'm a Client at port " + urlSplit[2]);
 
             //TODO nao usar allkeys (problema se este servidor em especifico nao tiver sido iniciado
-            string url = ConfigurationManager.AppSettings.AllKeys[0];
-
-            IServerService serverRemoteObject = ((IServerService)Activator.GetObject(typeof(IServerService), url));
 
             foreach (string serverUrl in ConfigurationManager.AppSettings.AllKeys) {
                 serverRemoteObjects.Add((IServerService)Activator.GetObject(typeof(IServerService), serverUrl));
             }
-            return serverRemoteObject;
+            return serverRemoteObjects;
         }
 
         public List<IServerService> getView(List<IServerService> view) {
@@ -80,20 +77,20 @@ namespace Client {
                 numServers = view.Count;
             }
             WaitHandle[] handles = new WaitHandle[numServers];
-            Console.WriteLine("Broadcasting to " + numServers + " servers...");
+            //Console.WriteLine("Broadcasting to " + numServers + " servers...");
             IAsyncResult[] asyncResults = new IAsyncResult[numServers]; //used when want to access IAsyncResult in index of handled that give the signal
             try {
                 for (int i = 0; i < numServers; i++) {
                     IServerService remoteObject = view[i];
                     requestViewDelegate viewDel = new requestViewDelegate(remoteObject.ViewRequest);
-                    Console.WriteLine(i);
+                    //Console.WriteLine(i);
                     IAsyncResult ar = viewDel.BeginInvoke(null, null);
                     asyncResults[i] = ar;
                     handles[i] = ar.AsyncWaitHandle;
                 }
                 int indxAsync = WaitHandle.WaitAny(handles, 300); //Wait for the first answer from the servers
                 if (indxAsync == WaitHandle.WaitTimeout) {
-                    Console.WriteLine("timeout with " + numServers.ToString());
+                    //Console.WriteLine("timeout with " + numServers.ToString());
                     getView(view);
                 }
                 else {
@@ -104,7 +101,7 @@ namespace Client {
                         if (servers.Count() != 0) {
                             List<IServerService> serverobjs = new List<IServerService>();
                             for (int j = 0; j < servers.Count; j++) {
-                                Console.WriteLine("answer from " + indxAsync + ": " + servers[j] + "(" + servers.Count + ")");
+                                //Console.WriteLine("answer from " + indxAsync + ": " + servers[j] + "(" + servers.Count + ")");
                                 serverobjs.Add((IServerService)Activator.GetObject(typeof(IServerService), servers[j]));
                             }
                             return serverobjs;
@@ -134,87 +131,3 @@ namespace Client {
         }
     }
 }
-
-/*
- 
-    
-        protected List<IServerService> prepareForRemoting(ref TcpChannel channel, string URL) {
-            string[] urlSplit = URL.Split(new Char[] { '/', ':' }, StringSplitOptions.RemoveEmptyEntries);
-            int port;
-            Int32.TryParse(urlSplit[2], out port);
-            channel = new TcpChannel(port); //Port can't be 10000 (PCS) neither 10001 (Puppet Master)
-            ChannelServices.RegisterChannel(channel, false);
-            myRemoteObject = new ClientService(this);
-            RemotingServices.Marshal(myRemoteObject, urlSplit[3], typeof(ClientService));
-            Console.WriteLine("Hello! I'm a Client at port " + urlSplit[2]);
-            foreach (string url in ConfigurationManager.AppSettings.AllKeys) {
-                serverRemoteObjects.Add((IServerService)Activator.GetObject(typeof(IServerService), url));
-            }
-            return serverRemoteObjects;
-        }
-
-        public List<IServerService> getView(List<IServerService> view) {
-            int numServers;
-            if (view.Count == 0) {
-                Console.WriteLine("No connections found. Broadcasting...");
-                view = serverRemoteObjects;
-                numServers = ConfigurationManager.AppSettings.AllKeys.Count(); //TODO use same file for client and server!!
-            }
-            else {
-                numServers = view.Count;
-            }
-            WaitHandle[] handles = new WaitHandle[numServers];
-            Console.WriteLine("Broadcasting to " + numServers + " servers...");
-            IAsyncResult[] asyncResults = new IAsyncResult[numServers]; //used when want to access IAsyncResult in index of handled that give the signal
-            try {
-                for (int i = 0; i < numServers; i++) {
-                    IServerService remoteObject = view[i];
-                    requestViewDelegate viewDel = new requestViewDelegate(remoteObject.ViewRequest);
-                    Console.WriteLine(i);
-                    IAsyncResult ar = viewDel.BeginInvoke(null, null);
-                    asyncResults[i] = ar;
-                    handles[i] = ar.AsyncWaitHandle;
-                }
-                int indxAsync = WaitHandle.WaitAny(handles, 300); //Wait for the first answer from the servers
-                if (indxAsync == WaitHandle.WaitTimeout) {
-                    Console.WriteLine("timeout with " + numServers.ToString());
-                    getView(view);
-                }
-                else {
-                    IAsyncResult asyncResult = asyncResults[indxAsync];
-                    requestViewDelegate viewDel = (requestViewDelegate)((AsyncResult)asyncResult).AsyncDelegate;
-                    try {
-                        List<string> servers = viewDel.EndInvoke(asyncResult);
-                        if (servers.Count() != 0) {
-                            List<IServerService> serverobjs = new List<IServerService>();
-                            for (int j = 0; j < servers.Count; j++) {
-                                Console.WriteLine("answer from " + indxAsync + ": " + servers[j] + "(" + servers.Count + ")");
-                                serverobjs.Add((IServerService)Activator.GetObject(typeof(IServerService), servers[j]));
-                            }
-                            return serverobjs;
-                        }
-                        else {
-                            return getView(view); //TODO? I dont like this
-                        }
-                    }
-                    catch (SocketException) {
-                        Console.WriteLine("ERROR: view is " + view.Count());
-                        Console.WriteLine("Server " + indxAsync + " is down. Restarting...");
-                        List<IServerService> newView = view;
-                        
-                        newView.RemoveAt(indxAsync);
-                        Console.WriteLine("Trying again with " + newView.Count);
-                        return getView(newView);
-                    }
-                    
-                }
-            } catch (SocketException e) {
-                Console.WriteLine("Connection error. Restarting...");
-                return getView(serverRemoteObjects); //TODO? isto acho que nao precisa de estar aqui (este catch)
-            }
-            Console.WriteLine("you shouldnt be here");
-            return null;
-        }
-    }
-
- * */
